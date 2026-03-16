4a224b2b0c613f1c896ca9b83b4b37472da83687
plastikfan <plastikfan@outlook.com>
2026-03-16 10:25:51 +0000

    chore(deps): fix lint and update deps and comments (#112)

--- internal/lab/test-utilities.go
+++ internal/lab/test-utilities.go
@@ -3,24 +3,29 @@ package lab
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
-	"runtime"
 	"strings"
 )
 
+// Path joins the relative path.
 func Path(parent, relative string) string {
 	segments := strings.Split(relative, "/")
 	return filepath.Join(append([]string{parent}, segments...)...)
 }
 
+// Normalise converts a path to use the correct separators for the native
+// file system.
 func Normalise(p string) string {
 	return strings.ReplaceAll(p, "/", string(filepath.Separator))
 }
 
+// Reason creates a standardised reason message for error messages.
 func Reason(name string) string {
 	return fmt.Sprintf("❌ for item named: '%v'", name)
 }
 
+// JoinCwd
 func JoinCwd(segments ...string) string {
 	if current, err := os.Getwd(); err == nil {
 		parent, _ := filepath.Split(current)
@@ -34,6 +39,7 @@ func JoinCwd(segments ...string) string {
 	panic("could not get root path")
 }
 
+// Root
 func Root() string {
 	if current, err := os.Getwd(); err == nil {
 		return current
@@ -42,19 +48,31 @@ func Root() string {
 	panic("could not get root path")
 }
 
-func Repo(relative string) string {
-	_, filename, _, _ := runtime.Caller(0) //nolint:dogsled // ignore
-	return Path(filepath.Dir(filename), relative)
-}
-
-func Log() string {
-	if current, err := os.Getwd(); err == nil {
-		parent, _ := filepath.Split(current)
-		grand := filepath.Dir(parent)
-		great := filepath.Dir(grand)
-
-		return filepath.Join(great, "Test", "test.log")
+// Combine creates a path from the parent combined with the relative path. The relative
+// path is a file system path so should only contain forward slashes, not the standard
+// file path separator as denoted by filepath.Separator, typically used when interacting
+// with the local file system. Do not use trailing "/".
+func Combine(parent, relative string) string {
+	if relative == "" {
+		return parent
 	}
 
-	panic("could not get root path")
+	return parent + "/" + relative
+}
+
+// Repo gets the root path of the repo using git and joins the relative path
+func Repo(relative string) string {
+	cmd := exec.Command("git", "rev-parse", "--show-toplevel")
+	output, _ := cmd.Output()
+
+	repo := strings.TrimSpace(string(output))
+
+	return Combine(repo, relative)
+}
+
+// Log get the log path
+func Log() string {
+	repo := Repo("")
+
+	return filepath.Join(repo, "test", "test.log")
 }
